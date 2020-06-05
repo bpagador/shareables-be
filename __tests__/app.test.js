c841928881d86f2664781574e14977afe7b7e3b8
Briseid Pagador <bpagador@gmail.com>
2020-06-04 17:26:28 -0700

    shows working list test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const request = require('supertest');
 const app = require('../lib/app');
-// const Shareable = require('../lib/models/Shareable');
+const Shareable = require('../lib/models/Shareable');
 
 
 
@@ -29,20 +29,42 @@ describe('app routes', () => {
         return request(app)
             .post('/shareables')
             .send({
-                url: 'http://pictureTHIS.com',
-                description: 'the best thing I have EVER seen',
+                user_name: 'Briseida',
+                affirmation: 'soar like an eagle',
             })
 
             .then(res => {
                 expect(res.body).toEqual({
                     _id: expect.anything(),
-                    url: 'http://pictureTHIS.com',
-                    description: 'the best thing I have EVER seen',
+                    user_name: 'Briseida',
+                    affirmation: 'soar like an eagle',
                     likes: 0,
                     __v: 0
                 });
             });
     });
 
-    
+    it('gets a list of affirmations', async() => {
+        await Shareable.create({
+            user_name: 'Ronald',
+            affirmation: 'you are the best'
+
+        });
+
+        return request(app)
+            .get('/shareables')
+            .then(res => {
+                expect(res.body).toEqual([
+                    {
+                        _id: expect.anything(),
+                        user_name: 'Ronald',
+                        affirmation: 'you are the best',
+                        likes: 0,
+                        __v: 0
+                    }
+                ]);
+            });
+    });
+
+
 });
